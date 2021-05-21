8b4f2a82d6d46a6d2116c9ea9979a0dee2f330c9
Carl Bostroem <cpbostrom@yahoo.com>
2021-05-21 05:50:15 -0500

    update

--- datalayerprovider/register_provider.py
+++ datalayerprovider/register_provider.py
@@ -33,43 +33,45 @@ connectionProvider = "tcp://boschrexroth:boschrexroth@127.0.0.1:2070"
 
 def run_provider(provider : datalayer.provider.Provider):
 
-    print("Starting provider...")
+    print("bostroemc: Starting provider...")
     provider_node = datalayerprovider.my_provider_node.MyProviderNode()
     with datalayer.provider_node.ProviderNode(provider_node.cbs, 1234) as node:
         result = provider.register_node("myData/myString", node)
         if result != datalayer.variant.Result.OK:
-            print("Register Data Provider failed with: ", result)
+            print("bostroemc: Register Data Provider failed with: ", result)
 
         result= provider.start()
         if result != datalayer.variant.Result.OK:
-            print("Starting Provider failed with: ", result)
+            print("bostroemc: Starting Provider failed with: ", result)
             
-        print("Provider started...")
-        print("Running endless loop...")
+        print("bostroemc: Provider started...")
+        print("bostroemc: Running endless loop...")
 
         count=0
         while True:
             count=count+1
             if count > 9:
                 break
-            time.sleep(1)
+            time.sleep(120)
         
         result = provider.stop()
+        print("bostroemc: Stopping provider loop...")
+
         if result != datalayer.variant.Result.OK:
-            print("Stopping Provider failed with: ", result)
+            print("bostroemc: Stopping Provider failed with: ", result)
         result = provider.unregister_node("myData/myString")
         if result != datalayer.variant.Result.OK:
-            print("Unregister Data Provider failed with: ", result)
+            print("bostroemc: Unregister Data Provider failed with: ", result)
 
 def run():
-    print("Simple Snap for ctrlX Datalayer Provider with Python")
-    print("Connect to ctrlX CORE: ", connectionProvider)
+    print("bostroemc: Simple Snap for ctrlX Datalayer Provider with Python")
+    print("bostroemc: Connect to ctrlX CORE: ", connectionProvider)
 
-    print("Create and start ctrlX Datalayer System")
+    print("bostroemc: Create and start ctrlX Datalayer System")
     with datalayer.system.System("") as datalayer_system:
         datalayer_system.start(False)
 
-        print("Creating provider...")
+        print("bostroemc: Creating provider...")
         with datalayer_system.factory().create_provider(connectionProvider) as provider:
 
             run_provider(provider)
