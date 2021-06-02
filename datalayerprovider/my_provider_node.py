a27ce7c3f60d83ea30d8ecaaef0c1bd1114c130c
Carl Bostroem <cpbostrom@yahoo.com>
2021-06-02 12:00:00 -0500

    i[date 1.1.0

--- datalayerprovider/my_provider_node.py
+++ datalayerprovider/my_provider_node.py
@@ -25,9 +25,22 @@ from datalayer.provider_node import ProviderNodeCallbacks, NodeCallback
 from datalayer.variant import Result, Variant
 
 import json
+import time
+from jsonschema import validate
 
 class NodePush:
     dataString: str = "Hello from Python Provider"
+    id : int = 0
+
+    schema = {
+        "type" : "object",
+        "properties" : {
+            "name" : {"type" : "array"},
+            "email" : {"type" : "string"},
+            "color" : {"type" : "array"},
+        },
+        "required" : ["name", "email", "color"]
+    }
     
     def __init__(self, queue):
         self.cbs = ProviderNodeCallbacks(
@@ -63,8 +76,21 @@ class NodePush:
         cb(Result(Result.OK), new_data)
     
     def __on_write(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
-        print("bostroemc: __on_write", data.get_string())
-        self.queue.append(data.get_string())
+        test = json.loads(data.get_string())
+
+        try:
+            validate(test, self.schema)
+            t = time.localtime()
+            test["time"] = [time.strftime("%H:%M:%S", t)]
+            test["time"].append("")
+ 
+            test["id"] = self.id
+            # self.queue.append(data.get_string())
+            self.queue.append(test)
+        except ValidationError as e:
+            print(e)       
+        
+        self.id+=1
         cb(Result(Result.OK), None)
 
     def __on_metadata(self, userdata: datalayer.clib.userData_c_void_p, address: str, cb: NodeCallback):
@@ -86,8 +112,50 @@ class NodePop:
         self.queue = queue
 
     def __on_create(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
-        print("__on_create")
         self.dataString
+        cb(Result(Result.OK), None)
+
+    def __on_remove(self, userdata: datalayer.clib.userData_c_void_p, address: str, cb: NodeCallback):
+        # Not implemented because no wildcard is registered
+        cb(Result(Result.UNSUPPORTED), None)
+
+    def __on_browse(self, userdata: datalayer.clib.userData_c_void_p, address: str, cb: NodeCallback):
+        new_data = Variant()
+        new_data.set_array_string([])
+        cb(Result(Result.OK), new_data)
+
+    def __on_read(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
+        new_data = Variant()
+        new_data.set_string(json.dumps(self.queue[0]) if len(self.queue)>0 else "")
+        cb(Result(Result.OK), new_data)
+    
+    def __on_write(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
+        new_data = Variant()
+
+        new_data.set_string(json.dumps(self.queue[0]) if len(self.queue)>0 else "")
+        if len(self.queue)>0: self.queue.pop(0)
+        cb(Result(Result.OK), new_data)
+
+    def __on_metadata(self, userdata: datalayer.clib.userData_c_void_p, address: str, cb: NodeCallback):
+        cb(Result(Result.OK), None)        
+
+class Node:
+    data: int = 0
+    
+    def __init__(self, queue):
+        self.cbs = ProviderNodeCallbacks(
+        self.__on_create,
+        self.__on_remove,
+        self.__on_browse,
+        self.__on_read,
+        self.__on_write,
+        self.__on_metadata
+        )
+        self.queue = queue
+
+    def __on_create(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
+        print("__on_create")
+        self.data
         cb(Result(Result.OK), None)
 
     def __on_remove(self, userdata: datalayer.clib.userData_c_void_p, address: str, cb: NodeCallback):
@@ -102,18 +170,15 @@ class NodePop:
         cb(Result(Result.OK), new_data)
 
     def __on_read(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
-        print("bostroemc: __on_read", userdata)
+        print("bostroemc: count __on_read", len(self.queue))
         new_data = Variant()
-        new_data.set_string(json.dumps(self.queue[0]))
+        new_data.set_uint32(len(self.queue))
         cb(Result(Result.OK), new_data)
     
     def __on_write(self, userdata: datalayer.clib.userData_c_void_p, address: str, data: Variant, cb: NodeCallback):
-        print("bostroemc: __on_write", data.get_string())
-        new_data = Variant()
-        new_data.set_string(json.dumps(self.queue[0]))
-        self.queue.pop(0)
-        cb(Result(Result.OK), new_data)
+        print("bostroemc: __on_write")
+        cb(Result(Result.OK), None)
 
     def __on_metadata(self, userdata: datalayer.clib.userData_c_void_p, address: str, cb: NodeCallback):
         print("__on_metadata")
-        cb(Result(Result.OK), None)        
+        cb(Result(Result.OK), None)              
